ac313ba8855580ebf9a189442f82a0e2783ee9fc
d0whc3r <1378986+d0whc3r@users.noreply.github.com>
2022-05-16 22:23:03 +0200

    fix: mock toLocaleString

--- packages/client/test/setup.mjs
+++ packages/client/test/setup.mjs
@@ -1,6 +1,15 @@
 import '@testing-library/jest-dom'
+import { afterAll, beforeAll, vi } from 'vitest'
 
 const noop = () => {
 }
 Object.defineProperty(window, 'scrollTo', { value: noop, writable: true })
 require('dotenv').config({ path: '.env.test' })
+
+let mockDate
+beforeAll(() => {
+  mockDate = vi.spyOn(Date.prototype, 'toLocaleString').mockReturnValue('2020-04-15, 14:46:50')
+})
+afterAll(() => {
+  mockDate.mockRestore()
+})
